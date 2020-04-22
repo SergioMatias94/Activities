edbe52bd424d33537fd4d559096350178659cb28
Enrique Promotor <isc.enprom@gmail.com>
2020-04-22 13:56:50 -0500

    Ejemplos MVC

---
+++ Activities_Proyect/Activities_Proyect/Controllers/EjemploController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Activities_Proyect.Controllers
+{
+    public class EjemploController : Controller
+    {
+        // GET: Ejemplo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public int promo()
+        {
+            return 1000;
+            
+        }
+    }
+}
